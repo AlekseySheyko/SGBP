72c444707ed45d785c947493c2345db395a618f8
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-01-08 15:41:06 +0200

    Custom list adapter implemented

---
+++ app/src/main/java/aleksey/sheyko/sgbp/models/Item.java
@@ -0,0 +1,82 @@
+package aleksey.sheyko.sgbp.models;
+
+import com.orm.SugarRecord;
+
+public class Item extends SugarRecord<Item> {
+    private int storeid;
+    private String name;
+    private String address;
+    private String phone;
+    private String latitude;
+    private String longitude;
+    private String category;
+
+    public Item() {
+    }
+
+    public Item(int storeid, String name, String address, String phone, String latitude, String longitude, String category) {
+        this.storeid = storeid;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.category = category;
+    }
+
+    public int getStoreid() {
+        return storeid;
+    }
+
+    public void setStoreid(int storeid) {
+        this.storeid = storeid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
--- app/src/main/java/aleksey/sheyko/sgbp/ui/fragments/NearestFragment.java
+++ app/src/main/java/aleksey/sheyko/sgbp/ui/fragments/NearestFragment.java
@@ -2,41 +2,60 @@ package aleksey.sheyko.sgbp.ui.fragments;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 import aleksey.sheyko.sgbp.R;
+import aleksey.sheyko.sgbp.adapters.ItemAdapter;
+import aleksey.sheyko.sgbp.models.Item;
 
 public class NearestFragment extends ListFragment {
 
-    public NearestFragment() {
-        //        int storeid = 1;
-        //        String name = "AR Performance";
-        //        String address = "9055 Locust Street";
-        //        String phone = "(916) 714-5277";
-        //        String latitude = "38.410609";
-        //        String longitude = "-121.364239";
-        //        String category = "Audio equipment";
-        //        Store store = new Store(id, name, address, phone, latitude, longitude, category);
-        //        store.save();
-
-//        List<Store> stores = Store.listAll(Store.class);
-//        for (Store store : stores) {
-//            Log.i("NearestFragment", store.getName());
-//        }
-    }
+    // declare class variables
+    private ArrayList<Item> m_parts = new ArrayList<Item>();
+    private Runnable viewParts;
+    private ItemAdapter m_adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-        // use your custom layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.rowlayout, R.id.label, values);
-        setListAdapter(adapter);
+
+        // instantiate our ItemAdapter class
+        // TODO: Maybe I'll be needed to change getActivity() to NearestFragment
+        m_adapter = new ItemAdapter(getActivity(), R.layout.list_item_store, m_parts);
+        setListAdapter(m_adapter);
+
+        // here we are defining our runnable thread.
+        viewParts = new Runnable(){
+            public void run(){
+                handler.sendEmptyMessage(0);
+            }
+        };
+
+        // here we call the thread we just defined - it is sent to the handler below.
+        Thread thread =  new Thread(null, viewParts, "MagentoBackground");
+        thread.start();
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+    private Handler handler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            // create some objects
+            // here is where you could also request data from a server
+            // and then create objects from that data.
+            m_parts.add(new Item(1, "Name #1", "Address #1", "Phone #1", "Latitude #1", "Longitude #1", "Category #1"));
+            m_parts.add(new Item(2, "Name #2", "Address #2", "Phone #2", "Latitude #2", "Longitude #2", "Category #1"));
+
+            m_adapter = new ItemAdapter(getActivity(), R.layout.list_item_store, m_parts);
+
+            // display the list.
+            setListAdapter(m_adapter);
+        }
+    };
 }
