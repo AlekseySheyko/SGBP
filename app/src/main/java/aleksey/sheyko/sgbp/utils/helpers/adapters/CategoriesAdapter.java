3df089e4ddb52d7a4fd1098a659d548269aba5ee
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-02-15 20:32:05 +0200

    Spinner dropdown color.

--- app/src/main/java/aleksey/sheyko/sgbp/utils/helpers/adapters/CategoriesAdapter.java
+++ app/src/main/java/aleksey/sheyko/sgbp/utils/helpers/adapters/CategoriesAdapter.java
@@ -26,7 +26,7 @@ public class CategoriesAdapter extends BaseAdapter {
         items.add(new Item(Constants.CATEGORY_BODY_CARE, R.drawable.category_body));
         items.add(new Item(Constants.CATEGORY_HOTELS, R.drawable.category_hotels));
         items.add(new Item(Constants.CATEGORY_MOBILE, R.drawable.category_mobile));
-        items.add(new Item(Constants.CATEGORY_OTHER, R.drawable.category_preview));
+        items.add(new Item(Constants.CATEGORY_OTHER, R.drawable.category_other));
     }
 
     @Override
