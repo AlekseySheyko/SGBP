69dd7cb7ca343b6139be79ca6021400c8ba67a56
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-03-02 13:09:51 +0200

    Participate request

--- app/src/main/java/aleksey/sheyko/sgbp/app/service/GeofenceService.java
+++ app/src/main/java/aleksey/sheyko/sgbp/app/service/GeofenceService.java
@@ -1,15 +1,31 @@
 package aleksey.sheyko.sgbp.app.service;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-import aleksey.sheyko.sgbp.app.tasks.SubmitParticipationTask;
+import aleksey.sheyko.sgbp.R;
+import aleksey.sheyko.sgbp.app.helpers.Constants;
+import aleksey.sheyko.sgbp.model.Notification;
+import aleksey.sheyko.sgbp.model.Store;
+import aleksey.sheyko.sgbp.rest.ApiService;
+import aleksey.sheyko.sgbp.rest.RestClient;
+import retrofit.ResponseCallback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class GeofenceService extends IntentService {
     public static final String TAG = GeofenceService.class.getSimpleName();
@@ -31,7 +47,29 @@ public class GeofenceService extends IntentService {
                 List<Geofence> geofences = event.getTriggeringGeofences();
                 for (Geofence geofence : geofences) {
                     String id = geofence.getRequestId();
-                    new SubmitParticipationTask(this, id).execute();
+                    String dateTime = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'").format(new Date());
+
+                    SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+                    String deviceId = sharedPrefs.getString("device_id", "");
+                    int userId = sharedPrefs.getInt("user_id", -1);
+                    int schoolId = sharedPrefs.getInt("school_id", -1);
+
+                    final Store store = Store.find(Store.class, "geofence_id = ?", id).get(0);
+                    int storeId = store.getStoreid();
+                    boolean isMobile;
+                    isMobile = store.getCategory().equals(Constants.CATEGORY_MOBILE);
+
+                    ApiService service = new RestClient().getApiService();
+                    service.participate(userId + "", userId, deviceId, schoolId, storeId, dateTime, isMobile, new ResponseCallback() {
+                        @Override public void success(Response response) {
+                            showNotification(store.getName());
+                            new Notification(store.getName(), getCurrentTime()).save();
+                        }
+
+                        @Override public void failure(RetrofitError e) {
+                            e.printStackTrace();
+                        }
+                    });
                 }
                 break;
             case Geofence.GEOFENCE_TRANSITION_EXIT:
@@ -39,5 +77,26 @@ public class GeofenceService extends IntentService {
                 break;
         }
         // List<Geofence> geofences = event.getTriggeringGeofences();
+    }
+
+    private void showNotification(String name) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Thanks for visiting")
+                        .setContentText(name + " participation accepted");
+        mBuilder.setAutoCancel(true);
+        // Sets an ID for the notification
+        int mNotificationId = 123;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    public String getCurrentTime() {
+        return new SimpleDateFormat("dd MMM, hh:mm", Locale.US)
+                .format(new Date()).toLowerCase();
     }
 }
--- app/src/main/java/aleksey/sheyko/sgbp/rest/ApiService.java
+++ app/src/main/java/aleksey/sheyko/sgbp/rest/ApiService.java
@@ -18,9 +18,9 @@ public interface ApiService {
 
     @FormUrlEncoded
     @POST("/SaveTeamMemberInfo")
-    void register(@Field("Key") String key, @Field("Device_Info_Id") int deviceId,
+    void register(@Field("Key") String key, @Field("Device_Info_Id") int userId,
                   @Field("First_Name") String firstName, @Field("Last_Name") String lastName,
-                  @Field("Device_UID") String userId, @Field("School_Id") int schoolId,
+                  @Field("Device_UID") String deviceId, @Field("School_Id") int schoolId,
                   @Field("User_Email") String email, @Field("User_Type") int userType,
                   @Field("Has_Multiple_Child") boolean multiGrade, @Field("Is_User_Registered") boolean isRegistered,
                   @Field("Is_Coupon_Allowed") boolean receiveCoupons, @Field("Is_Notification_Allowed") boolean getNotifications,
@@ -38,4 +38,11 @@ public interface ApiService {
 
     @GET("/GetUserReginfoBYDevice_UID")
     void checkRegistration(@Query("Device_UID") String deviceId, Callback<Response> callback);
+
+    @FormUrlEncoded
+    @POST("/SaveProgramParticipationByDevice")
+    void participate(@Field("Key") String userId, @Field("Device_Info_Id") int deviceId,
+                     @Field("Device_UID") String deviceId2, @Field("School_Id") int schoolId,
+                     @Field("Store_Id") int storeId, @Field("Participation_DateTime") String dateTime,
+                     @Field("Clicked_To_Participate") boolean isMobile, Callback<Response> callback);
 }
